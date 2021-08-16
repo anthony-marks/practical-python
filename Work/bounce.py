c2690e8a4ee247c2d76748561e047837de8d5e59
Anthony Marks <marks.anthony@gmail.com>
2021-08-16 23:20:04 +1000

    added sears and ball bounces

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+REBOUND = 3/5        # how much it bounces back
+NUMBER_BOUNCES = 10  # number of bounces we measure
+height = 100         # initial height in metres
+
+bounce_count = 1
+while bounce_count <= NUMBER_BOUNCES:
+    height = height * REBOUND
+    print(bounce_count, round(height, 4))
+    bounce_count = bounce_count + 1
+
+
+
---
+++ Work/resources/links.md
@@ -0,0 +1,5 @@
+[Practical Python Programming](https://dabeaz-course.github.io/practical-python/Notes/Contents.html)
+
+[Want to discuss the course? You can join the conversation on Gitter](https://gitter.im/dabeaz-course/practical-python) 
+
+[Notes to Instructors](https://dabeaz-course.github.io/practical-python/Notes/InstructorNotes.html)
